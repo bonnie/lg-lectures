c19633bacd90e1e32c2f1c8c5835e360764bf50d
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-08-28 16:44:21 -0700

    Update so invalid command tests pass.

--- tdd/shopping_list.js
+++ tdd/shopping_list.js
@@ -14,3 +14,20 @@
 const command = process.argv[2]
 const store = process.argv[3]
 const item = process.argv.slice(4).join(' ')
+
+switch (command) {
+  case 'list':
+    break;
+  case 'add':
+    break;
+  case 'delete':
+    break;
+  default:
+    console.error(`USAGE:
+      *    shopping_list.js <command> <args>
+      *  where command/args combos can be:
+      *    list
+      *    list <store>
+      *    add <store> <item>
+      *    delete <store> <item>`)
+}
